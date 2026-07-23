5db2c63ed230f098f3b2aa56f2301054c80513b4
Sarthak <sarthakbhadouriya@gmail.com>
2026-07-23 19:02:53 +0530

    Add greetUser function to log personalized greeting

--- 03. JavaScript Fundamentals/01. Build a Counter App/29. JavaScript challenges/02. Concatenate two strings in a function/index.js
+++ 03. JavaScript Fundamentals/01. Build a Counter App/29. JavaScript challenges/02. Concatenate two strings in a function/index.js
@@ -3,3 +3,9 @@ let greeting = "Hi there"
 
 // Create a function that logs out "Hi there, Linda!" when called
 
+function greetUser(name) {
+    console.log(`${greeting}, ${name}!`);
+}
+
+// Call the function with the name variable
+greetUser(name);
